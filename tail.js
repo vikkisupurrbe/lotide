752c0d73355572d512be9df7cb952873e722baba
Vikki <yingjialiu1216@gmail.com>
2025-02-18 16:17:36 -0800

    added tail.js

---
+++ tail.js
@@ -0,0 +1,31 @@
+// function implementation
+const tail = function(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  } else {
+    return arr.slice(1);
+  }
+};
+
+const assertEqual = function(actual, expected) {
+  let newArr = tail(actual); // Store the processed array
+  
+  console.log(newArr);
+
+  if (newArr.length !== expected.length) {
+    return `🛑🛑🛑 Assertion Failed: Arrays have different lengths`;
+  }
+
+  for (let i = 0; i < newArr.length; i++) {
+    if (newArr[i] !== expected[i]) {
+      return `🛑🛑🛑 Assertion Failed: ${newArr} !== ${expected}`;
+    }
+  }
+
+  return `✅✅✅ Assertion Passed: ${newArr} === ${expected}`;
+};
+
+// Testing function
+console.log(assertEqual(([5, 6, 7]), [6, 7]));
+console.log(assertEqual(([5, 5, 6, 8]), [5, 6, 7]));
+console.log(assertEqual([], [6, 7]));
